e53b28205545e59aea6e3f33fca7faf32215a9bf
yclee <lancelee92@gmail.com>
2019-03-12 07:46:04 +0800

    added templates and new menu item function

--- vagrant/myWebServer/project.py
+++ vagrant/myWebServer/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
@@ -44,21 +44,28 @@ def HelloWorld():
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
+    # output = ''
+    # for i in items:
+    #     output += i.name
+    #     output += '</br>'
+    #     output += i.price
+    #     output += '</br>'
+    #     output += i.description
+    #     output += '</br>'
+    #     output += '</br>'
 
-    return output
+    return render_template('menu.html',restaurant=restaurant, items = items)
 
-@app.route('/restaurants/<int:restaurant_id>/create/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form['price'], restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('newmenuitem.html',restaurant_id = restaurant_id)
+    #return "page to create a new menu item. Task 1 complete!"
     #return render_template('menu.html',restaurant=restaurant, items = items)
 
 # Task 2: Create route for editMenuItem function here
