39c8859bcecb8a4c6f998b52d697e87517a853f8
yclee <lancelee92@gmail.com>
2019-03-13 22:30:08 +0800

    added flash message, styling and jsonify

--- vagrant/myWebServer/database_setup.py
+++ vagrant/myWebServer/database_setup.py
@@ -14,6 +14,14 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -25,6 +33,19 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'price': self.price,
+            'course': self.course,
+            'restaurant_id': self.restaurant_id,
+            'restaurant_id': self.restaurant_id,
+        }
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
--- vagrant/myWebServer/project.py
+++ vagrant/myWebServer/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
@@ -40,6 +40,20 @@ def HelloWorld():
 
     return output
 
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+# ADD JSON ENDPOINT HERE
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+    menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(MenuItem=menuItem.serialize)
+
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
@@ -62,6 +76,7 @@ def newMenuItem(restaurant_id):
         newItem = MenuItem(name = request.form['name'], description = request.form['description'], price = request.form['price'], restaurant_id = restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("new menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     else:
         return render_template('newmenuitem.html',restaurant_id = restaurant_id)
@@ -81,6 +96,7 @@ def editMenuItem(restaurant_id, menu_id):
         item.price = request.form['price']
         session.add(item)
         session.commit()
+        flash("menu item edited!")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     else:
         return render_template('editMenuItem.html',restaurant=restaurant, item = item)
@@ -98,11 +114,13 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(item)
         session.commit()
+        flash(" menu item deleted!")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     else:
         return render_template('deleteMenuItem.html',restaurant=restaurant, item = item)
     #return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug=True
     app.run(host = '0.0.0.0', port = 5000)
